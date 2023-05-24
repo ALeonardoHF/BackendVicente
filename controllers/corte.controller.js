243d921c34e6a36a89d1e74689052df0a795ebd6
ALeonardoHF <leonardo_heredia123@hotmail.com>
2023-05-24 00:10:13 -0600

    all

--- controllers/corte.controller.js
+++ controllers/corte.controller.js
@@ -9,25 +9,25 @@ const Op = db.Sequelize.Op;
 
 exports.findAllRegistros = async (req, res) => {
   // Obtener la fecha actual
-  const currentDate1 = new Date();
-  const currentDate = new Date(currentDate1);
-  currentDate.setDate(currentDate1.getDate() -1);
+  const currentDate2 = new Date();
+  const currentDateR = new Date(currentDate2);
+  currentDateR.setDate(currentDate1.getDate() -1);
 
   // Obtener la fecha de ayer
-  const yesterdayDate = new Date(currentDate);
-  yesterdayDate.setDate(currentDate.getDate() - 1);
+  const yesterdayDate = new Date(currentDateR);
+  yesterdayDate.setDate(currentDateR.getDate() - 1);
 
   // Establecer la hora a las 10:00 PM
   yesterdayDate.setHours(22, 0, 0, 0);
-  currentDate.setHours(22, 0, 0, 0);
+  currentDateR.setHours(22, 0, 0, 0);
 
   // Formatear las fechas al formato deseado
   const formattedYesterday = yesterdayDate.toISOString().slice(0, 19).replace('T', ' ');
-  const formattedToday = currentDate.toISOString().slice(0, 19).replace('T', ' ');
+  const formattedToday = currentDateR.toISOString().slice(0, 19).replace('T', ' ');
 
   Registro.findAll({
     attributes: [
-      [sequelize.fn('SUM', sequelize.col('Precio')), 'totalPrecio']
+      [sequelize.fn('SUM', sequelize.col('Precio')), 'totalPrecioRegistros']
     ],
     where: {
       CheckIn: {
