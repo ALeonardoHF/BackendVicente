86df34a692d163bc2fe4769bc3288f22608497db
ALeonardoHF <leonardo_heredia123@hotmail.com>
2023-05-28 13:08:52 -0600

    corte ventas

---
+++ controllers/corteVentas.controller.js
@@ -0,0 +1,45 @@
+const sequelize = require('sequelize');
+const db = require("../models");
+const Venta = db.venta;
+
+exports.findCorteVentas = async (req, res) => {
+    // Obtener la fecha actual
+    const currentDate1 = new Date();
+    const currentDate = new Date(currentDate1);
+    currentDate.setDate(currentDate1.getDate() -1);
+  
+    // Obtener la fecha de ayer
+    const yesterdayDate = new Date(currentDate);
+    yesterdayDate.setDate(currentDate.getDate() - 1);
+  
+    // Establecer la hora a las 10:00 PM
+    yesterdayDate.setHours(22, 0, 0, 0);
+    currentDate.setHours(22, 0, 0, 0);
+  
+    // Formatear las fechas al formato deseado
+    const formattedYesterday = yesterdayDate.toISOString().slice(0, 19).replace('T', ' ');
+    const formattedToday = currentDate.toISOString().slice(0, 19).replace('T', ' ');
+  
+    Venta.findAll({
+      attributes: [
+        [sequelize.fn('SUM', sequelize.col('Precio')), 'totalVentas']
+      ],
+      where: {
+        CheckIn: {
+          [Op.between]: [formattedYesterday, formattedToday]
+        }
+      }
+    })
+      .then(data => {
+        console.log('data :>> ', data[0].getDataValue('totalVentas'));
+        res.send(data[0].getDataValue('totalVentas'));
+  
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving total ventas."
+        });
+      });
+  
+  };
